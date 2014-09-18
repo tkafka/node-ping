c8a1bf02c45f0c7a559cc139b4d92046f35f668d
Tomas Kafka <tk@tomaskafka.com>
2014-09-18 13:58:13 +0200

    kept only promises, added timing

--- examples/example.js
+++ examples/example.js
@@ -4,9 +4,9 @@ var ping = require('../index');
 
 var hosts = ['192.168.1.1', 'google.com', 'yahoo.com'];
 hosts.forEach(function(host){
-    ping.sys.probe(host, function(isAlive){
-        var msg = isAlive ? 'host ' + host + ' is alive' : 'host ' + host + ' is dead';
-        console.log(msg);
-    });
+    ping.promise.probe(host)
+    	.then(function(res) {
+    		console.log(host, res);
+    	})
 });
 
